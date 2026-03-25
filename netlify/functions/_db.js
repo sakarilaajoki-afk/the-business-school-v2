3b7d6074a18368cae91887f9940ae270cf587b78
sakarilaajoki-afk <sakari.laajoki@gmail.com>
2026-03-25 09:23:39 +0200

    add _db.js

---
+++ netlify/functions/_db.js
@@ -0,0 +1,32 @@
+const { neon } = require('@neondatabase/serverless');
+
+let _sql = null;
+
+function getDb() {
+  if (!_sql) {
+    if (!process.env.DATABASE_URL) {
+      throw new Error('DATABASE_URL environment variable is not set');
+    }
+    _sql = neon(process.env.DATABASE_URL);
+  }
+  return _sql;
+}
+
+function cors(body, status = 200) {
+  return {
+    statusCode: status,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+    },
+    body: JSON.stringify(body),
+  };
+}
+
+function handleOptions() {
+  return { statusCode: 204, headers: { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'Content-Type, Authorization', 'Access-Control-Allow-Methods': 'GET, POST, OPTIONS' } };
+}
+
+module.exports = { getDb, cors, handleOptions };
